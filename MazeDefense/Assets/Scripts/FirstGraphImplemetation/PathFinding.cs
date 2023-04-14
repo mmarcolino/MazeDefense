64b6af974971359e143f33143a8c0b3625ddfede
Matheus <matheus.marcolino.1320642@sga.pucminas.br>
2023-04-13 21:43:07 -0300

    adding graph first implementation

---
+++ MazeDefense/Assets/Scripts/FirstGraphImplemetation/PathFinding.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFinding : MonoBehaviour
+{
+
+    public Transform seeker, target;
+    Grid grid;
+    Node seekerNode, targetNode;
+    public GameObject GridOwner;
+
+
+    void Start()
+    {
+        //Instantiate grid
+        grid = GridOwner.GetComponent<Grid>();
+    }
+
+
+    public void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        //get player and target position in grid coords
+        seekerNode = grid.NodeFromWorldPoint(startPos);
+        targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(seekerNode);
+
+        //calculates path for pathfinding
+        while (openSet.Count > 0)
+        {
+
+            //iterates through openSet and finds lowest FCost
+            Node node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].FCost <= node.FCost)
+                {
+                    if (openSet[i].hCost < node.hCost)
+                        node = openSet[i];
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            //If target found, retrace path
+            if (node == targetNode)
+            {
+                RetracePath(seekerNode, targetNode);
+                return;
+            }
+
+            //adds neighbor nodes to openSet
+            foreach (Node neighbour in grid.GetNeighbors(node))
+            {
+                if (neighbour.obstacle || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+    }
+
+    //reverses calculated path so first node is closest to seeker
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+
+    }
+
+    //gets distance between 2 nodes for calculating cost
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.GridX - nodeB.GridX);
+        int dstY = Mathf.Abs(nodeA.GridY - nodeB.GridY);
+
+        if (dstX > dstY)
+            return 14 * dstY + 10 * (dstX - dstY);
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+}
--- MazeDefense/Assets/Scripts/SecondGraphImplemetation/WorldTile.cs
+++ MazeDefense/Assets/Scripts/SecondGraphImplemetation/WorldTile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class WorldTile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
